cd243c16775e728be488f2cd776b7da3536d81e9
zzj <156321781@qq.com>
2024-01-19 11:55:36 +0800

    轮播图api数据渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,17 +2,21 @@
 import { getHomeBannerAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
 import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { BannerItem } from '@/types/home'
 
+const list = ref<BannerItem[]>([])
 const getHomeBanner = async () => {
   const res = await getHomeBannerAPI()
-  console.log("结果：",res.result)
+  console.log('结果：', res.result)
+  list.value = res.result
 }
 onLoad(() => getHomeBanner())
 </script>
 
 <template>
   <CustomNavbar></CustomNavbar>
-  <XtxSwiper></XtxSwiper>
+  <XtxSwiper :list="list"></XtxSwiper>
   <view class="index">index2</view>
 </template>
 
