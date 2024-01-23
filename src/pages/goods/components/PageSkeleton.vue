a08b28260d9d858d2fd44b7aada252492439d35b
zzj <156321781@qq.com>
2024-01-23 15:22:58 +0800

    商品详情页骨架屏

---
+++ src/pages/goods/components/PageSkeleton.vue
@@ -0,0 +1,232 @@
+<template name="skeleton">
+  <view class="sk-container">
+    <scroll-view scroll-y="true" class="viewport viewport" enable-back-to-top="true">
+      <view class="goods goods">
+        <view class="preview preview">
+          <swiper circular="true" current="0" autoplay="false">
+            <swiper-item
+              style="
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                transform: translate(0%, 0px) translateZ(0px);
+              "
+            >
+              <image mode="aspectFill" class="sk-image"></image>
+            </swiper-item>
+          </swiper>
+          <view class="indicator indicator">
+            <text class="current sk-transparent sk-opacity">1</text>
+            <text class="split sk-transparent sk-opacity">/</text>
+            <text class="total sk-transparent sk-opacity">5</text>
+          </view>
+        </view>
+        <view class="meta meta">
+          <view class="price price">
+            <text class="symbol sk-transparent sk-opacity">¥</text>
+            <text class="number sk-transparent sk-text-14-2857-707 sk-text">238.00</text>
+          </view>
+          <view class="name ellipsis sk-transparent sk-text-14-2857-796 sk-text"
+            >多米尼加陈年朗姆酒700毫升</view
+          >
+          <view class="desc sk-transparent sk-text-0-0000-831 sk-text">陈年朗姆，香味芬芳app</view>
+        </view>
+        <view class="action action">
+          <view class="item arrow sk-pseudo sk-pseudo-circle">
+            <text class="label sk-transparent sk-text-14-2857-857 sk-text">选择</text>
+            <text class="text ellipsis sk-transparent sk-text-14-2857-674 sk-text">
+              请选择商品规格
+            </text>
+          </view>
+          <view class="item arrow sk-pseudo sk-pseudo-circle">
+            <text class="label sk-transparent sk-text-14-2857-747 sk-text">送至</text>
+            <text class="text ellipsis sk-transparent sk-text-14-2857-910 sk-text">
+              请选择收获地址
+            </text>
+          </view>
+          <view class="item arrow sk-pseudo sk-pseudo-circle"></view>
+        </view>
+      </view>
+    </scroll-view>
+    <view class="toolbar toolbar" style="padding-bottom: 0px">
+      <view class="icons icons">
+        <button class="icons-button sk-transparent sk-button sk-pseudo sk-pseudo-circle">
+          <text class="icon-heart sk-pseudo sk-pseudo-circle"></text>收藏
+        </button>
+        <button
+          class="icons-button sk-transparent sk-button sk-pseudo sk-pseudo-circle"
+          open-type="contact"
+        >
+          <text class="icon-handset sk-pseudo sk-pseudo-circle"></text>客服
+        </button>
+        <navigator class="icons-button sk-transparent" open-type="switchTab">
+          <text class="icon-cart sk-pseudo sk-pseudo-circle"></text>购物车
+        </navigator>
+      </view>
+      <view class="buttons buttons">
+        <view
+          class="addcart sk-transparent sk-text-31-9444-10 sk-text"
+          style="background-position-x: 50%"
+        >
+          加入购物车
+        </view>
+        <view
+          class="buynow sk-transparent sk-text-31-9444-438 sk-text"
+          style="background-position-x: 50%"
+        >
+          立即购买
+        </view>
+      </view>
+    </view>
+    //弹出层
+  </view>
+</template>
+
+<style>
+/*
+此文件为开发者工具生成，生成时间: 2024/1/23下午3:17:24
+
+在 C:\Users\子子杰\Desktop\webstudy_qf\mystudy\heima-小兔先-uni\mydemo\heima-shop\dist\dev\mp-weixin\pages\goods\goods.wxss 中引入样式
+```
+@import "./goods.skeleton.wxss";
+```
+
+更多详细信息可以参考文档：https://developers.weixin.qq.com/miniprogram/dev/devtools/skeleton.html
+*/
+.sk-transparent {
+  color: transparent !important;
+}
+.sk-opacity {
+  opacity: 0 !important;
+}
+.sk-text-14-2857-707 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 78.4rpx;
+  position: relative !important;
+}
+.sk-text {
+  background-origin: content-box !important;
+  background-clip: content-box !important;
+  background-color: transparent !important;
+  color: transparent !important;
+  background-repeat: repeat-y !important;
+}
+.sk-text-14-2857-796 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 44.8rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-831 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 24rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-857 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-674 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-747 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-910 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-31-9444-10 {
+  background-image: linear-gradient(
+    transparent 31.9444%,
+    #eeeeee 0%,
+    #eeeeee 68.0556%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 72rpx;
+  position: relative !important;
+}
+.sk-text-31-9444-438 {
+  background-image: linear-gradient(
+    transparent 31.9444%,
+    #eeeeee 0%,
+    #eeeeee 68.0556%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 72rpx;
+  position: relative !important;
+}
+.sk-button {
+  color: #efefef !important;
+  background: #efefef !important;
+  border: none !important;
+  box-shadow: none !important;
+}
+.sk-image {
+  background: #efefef !important;
+}
+.sk-pseudo::before,
+.sk-pseudo::after {
+  background: #efefef !important;
+  background-image: none !important;
+  color: transparent !important;
+  border-color: transparent !important;
+}
+.sk-pseudo-rect::before,
+.sk-pseudo-rect::after {
+  border-radius: 0 !important;
+}
+.sk-pseudo-circle::before,
+.sk-pseudo-circle::after {
+  border-radius: 50% !important;
+}
+.sk-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: transparent;
+}
+</style>
