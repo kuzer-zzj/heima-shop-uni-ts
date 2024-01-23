691d3d242a4b2d054837e273981f477c9ab9f268
zzj <156321781@qq.com>
2024-01-23 11:57:04 +0800

    商品图片切换&大图预览交互

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -19,6 +19,17 @@ const getGoodsById = async () => {
 onLoad(() => {
   getGoodsById()
 })
+
+const currentIndex = ref(0)
+const onChange: UniHelper.SwiperOnChange = (e) => {
+  currentIndex.value = e.detail.current
+}
+const onImgTap = (item: string) => {
+  uni.previewImage({
+    current: item,
+    urls: goods.value!.mainPictures,
+  })
+}
 </script>
 
 <template>
@@ -27,15 +38,15 @@ onLoad(() => {
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
+        <swiper circular @change="onChange">
           <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" />
+            <image @tap="onImgTap(item)" mode="aspectFill" :src="item" />
           </swiper-item>
         </swiper>
         <view class="indicator">
-          <text class="current">1</text>
+          <text class="current">{{ currentIndex + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">5</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
