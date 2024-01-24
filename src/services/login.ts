dd77e15f3046f8abeb9d26fa306c6ef1053be79a
zzj <156321781@qq.com>
2024-01-24 11:35:25 +0800

    微信登录

---
+++ src/services/login.ts
@@ -0,0 +1,19 @@
+import type { LoginResult } from '@/types/member'
+import { http } from '@/utils/http'
+
+type LoginParams = {
+  code: string
+  encryptedData: string
+  iv: string
+}
+/**
+ * 小程序登录
+ * @param data 请求参数
+ */
+export const postLoginWxMinAPI = (data: LoginParams) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/login/wxMin',
+    data,
+  })
+}
