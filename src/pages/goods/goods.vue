3bf83f1127515d864150978cf09c900758432938
zzj <156321781@qq.com>
2024-01-29 16:12:24 +0800

    加一个购物车页面详情页跳转可以有返回操作

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -194,7 +194,7 @@ const onOpenPopup = (type: 'addr' | 'service') => {
         <button class="icons-button" open-type="contact">
           <text class="icon-handset"></text>客服
         </button>
-        <navigator class="icons-button" url="/pages/cart/cart" open-type="switchTab">
+        <navigator class="icons-button" url="/pages/cart/cart2" open-type="navigate">
           <text class="icon-cart"></text>购物车
         </navigator>
       </view>
